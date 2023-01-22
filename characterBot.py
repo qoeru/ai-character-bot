28fbd1342f726bf129cd5863dbb8ee54b6456c4c
Qoeru <qoeru.q034u@gmail.com>
2023-01-22 13:45:45 +0300

    little fixes

--- characterBot.py
+++ characterBot.py
@@ -1,3 +1,4 @@
+import os
 import telebot
 import openai
 
@@ -22,13 +23,12 @@ restart_sequence = "\n\nPerson:"
 
 @bot.message_handler(content_types=['text'])
 def handle_welcome(message):
-    chat_log = character_description
-    prompt_text = f'{chat_log}{restart_sequence} {message.text}{start_sequence}'
+    prompt_text = f'{character_description}{restart_sequence}{message.text}{start_sequence}'
     response = openai.Completion.create(
       model="text-davinci-003",
       prompt=prompt_text,
       temperature=0.8,
-      max_tokens=150,
+      max_tokens=200,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0.3,
@@ -36,7 +36,7 @@ def handle_welcome(message):
     )
     story = response['choices'][0]['text']
     msg = bot.send_message(message.from_user.id, story)
-    chat_log = append_interaction_to_chat_log(prompt_text, story, chat_log) 
+    chat_log = append_interaction_to_chat_log(message.text, story, "") 
     bot.register_next_step_handler(msg, handle_message, chat_log)
 
 def handle_message(message, chat_log):
@@ -45,21 +45,24 @@ def handle_message(message, chat_log):
       model="text-davinci-003",
       prompt=prompt_text,
       temperature=0.8,
-      max_tokens=150,
+      max_tokens=1000,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0.3,
       stop=["\n"],
     )
     story = response['choices'][0]['text']
-    bot.send_message(message.from_user.id, story)
-    chat_log = append_interaction_to_chat_log(prompt_text, story, chat_log)
+    msg = bot.send_message(message.from_user.id, story)
+    chat_log = append_interaction_to_chat_log(message.text, story, chat_log)
+    bot.register_next_step_handler(msg, handle_message, chat_log)
 
 def append_interaction_to_chat_log(question, answer, chat_log):
-    if chat_log is None:
-        chat_log = character_description
-    text = f'{restart_sequence}{question}{start_sequence}{answer}{chat_log}'
-    return text[-100:]
+    if(len(chat_log) > 9000):
+        chat_log = chat_log[-9000:]
+    context = f'{character_description} "\n" {chat_log} {restart_sequence}{question} {start_sequence}{answer} '
+    print(context)
+    return context
+
 
 if __name__ == '__main__':
-    bot.infinity_polling()
+    bot.polling()
